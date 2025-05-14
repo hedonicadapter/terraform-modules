b3be7c9ab52bc68e9473ad2f3fa95ab6a7d1fed4
hedonicadapter <sam.herman@xenit.se>
2025-05-14 13:49:42 +0200

    pin to later version

--- main.tf
+++ main.tf
@@ -3,7 +3,8 @@ terraform {
 
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
     }
     azapi = {
       source  = "azure/azapi"
